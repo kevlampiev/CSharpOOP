91832cd707a9796e29b6711efc2d2944d4a864fe
kevlampiev <kevlampiev@gmail.com>
2022-08-31 02:25:46 +0700

    Modified properties

--- Lesson2/BankAccount.cs
+++ Lesson2/BankAccount.cs
@@ -17,29 +17,17 @@ namespace Lesson2
     internal class BankAccount
     {
         //Поля
-        private readonly Int64 accountNumber;
+        private Int64 accountNumber;
         private float balance = 0;
-        private readonly AccountTypes accountType = AccountTypes.DemandDeposit;
+        private AccountTypes accountType = AccountTypes.DemandDeposit;
 
         private static Int64 defaultAccountNo = 0;
 
-        public string AccountNumber { get { return accountNumber.ToString(); } }
-        public string AccountType { get { return accountType.ToString(); } }
+        public long AccountNumber { get { return accountNumber; } set { accountNumber = value; } }
+        public AccountTypes AccountType { get { return accountType; } set { accountType = value; } }
         public float Balance { get { return balance; } set { if (value > 0) { balance = value; } } }
 
-/*        public BankAccount(Int64 accountNo, AccountTypes accountType, float balance) 
-        { 
-            this.accountNumber = accountNo;
-            this.accountType = accountType;
-            this.balance = (balance>0)?balance:0; 
-        }
 
-        public BankAccount(long accountNumber, float balance, AccountTypes accountType)
-        {
-            this.accountNumber = accountNumber;
-            this.accountType = accountType;
-        }
-*/
         /// <summary>
         /// Выводит данные о счете и его состоянии
         /// </summary>
--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -6,14 +6,15 @@ namespace Lesson2
     {
         static void Main(string[] args)
         {
-            BankAccount account1 = new BankAccount();
-            BankAccount account2 = new BankAccount(AccountTypes.CreditCardAccount);
-            BankAccount account3 = new BankAccount((Int64)148524632841);
-            BankAccount account4 = new BankAccount((float)1100);
-            account1.Print();
-            account2.Print();
-            account3.Print();
-            account4.Print();
+            BankAccount account = new BankAccount();
+            Console.WriteLine("Standard contructor");
+            account.Print();
+            account.AccountNumber = 148777;
+            account.AccountType = AccountTypes.CreditCardAccount;
+            account.Balance = -500;
+            Console.WriteLine("After properties modification");
+            account.Print();
+
             Console.ReadKey();
         }
     }
