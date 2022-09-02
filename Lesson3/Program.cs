fbddedb30e5709b00517f4ba03ddf4b8fc13f802
kevlampiev <kevlampiev@gmail.com>
2022-09-03 01:02:35 +0700

    point 1. Added a new method to Bank account to make transactions between accounts

---
+++ Lesson3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lesson3
+{
+    internal class Program
+    {
+        static void DisplayHeader(string header)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(header);
+            Console.WriteLine(new string('-', 80));
+            Console.ResetColor();
+
+        }
+
+        static void PointOne()
+        {
+            DisplayHeader("Задание 1. Создание двух счетов, списание с одного и зачисление на второй");
+            ExtendedBankAccount account1 = new ExtendedBankAccount(600);
+            ExtendedBankAccount account2 = new ExtendedBankAccount(100);
+            Console.WriteLine("Счет списания до перевода");
+            account1.Print();
+            Console.WriteLine("Счет зачисления до перевода");
+            account2.Print();
+
+            account2.TopUpFromAccount(account1, 400);
+            Console.WriteLine("Счет списания после перевода");
+            account1.Print();
+            Console.WriteLine("Счет зачисления после перевода");
+            account2.Print();
+        }
+
+        static void Main(string[] args)
+        {
+            PointOne();
+            Console.ReadKey();
+        }
+    }
+}
