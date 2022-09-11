aa24be048ed097a491371286ff0e63172bd0c549
kevlampiev <kevlampiev@gmail.com>
2022-09-11 15:53:52 +0700

    Point 2. Added imaginary numbers

--- Lesson5/Program.cs
+++ Lesson5/Program.cs
@@ -4,9 +4,18 @@ namespace Lesson5
 {
     internal class Program
     {
+        static void DisplayHeader(string header)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(header);
+            Console.WriteLine(new string('-', 80));
+            Console.ResetColor();
+
+        }
+
         static void PointOne()
         {
-            Console.WriteLine("Задание 1. Класс рациональных чисел");
+            DisplayHeader("Задание 1. Класс рациональных чисел");
             RationalNumber numberOne = new RationalNumber(4, 6);
             RationalNumber numberTwo = new RationalNumber(5, 7);
             RationalNumber numberThree = new RationalNumber(2, 3);
@@ -27,10 +36,26 @@ namespace Lesson5
             Console.WriteLine($"Increment of {numberOne.ToString()} is {(++numberOne).ToString()}");
         }
 
+        static void PointTwo() 
+        {
+            DisplayHeader("Задание 2. Класс комплексных чисел");
+            ImaginaryNumber a = new ImaginaryNumber(4, 6);
+            ImaginaryNumber b = new ImaginaryNumber(1, - 7.5);
+            Console.WriteLine($" a = {a.ToString()}");
+            Console.WriteLine($" b = {b.ToString()}");
+            Console.WriteLine($" a == b? = {a == b}");
+            Console.WriteLine($" a != b? = {a != b}");
+            Console.WriteLine($" a-b = {(a - b).ToString()}");
+            Console.WriteLine($" a+b = {(a + b).ToString()}");
+            Console.WriteLine($" a*b = {(a * b).ToString()}");
+            Console.WriteLine($" a/b = {(a / b).ToString()}");
+        }
+
         static void Main(string[] args)
         {
 
             PointOne();
+            PointTwo();
             Console.ReadKey();
         }
     }
