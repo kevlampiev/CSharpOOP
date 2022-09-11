a5b521293795a54ee8cf437df934f1b44925738f
kevlampiev <kevlampiev@gmail.com>
2022-09-11 14:20:49 +0700

    Point one. Rational number class

---
+++ Lesson5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lesson5
+{
+    internal class Program
+    {
+        static void PointOne()
+        {
+            Console.WriteLine("Задание 1. Класс рациональных чисел");
+            RationalNumber numberOne = new RationalNumber(4, 6);
+            RationalNumber numberTwo = new RationalNumber(5, 7);
+            RationalNumber numberThree = new RationalNumber(2, 3);
+
+            Console.WriteLine($"Firts rational number {numberOne}");
+            Console.WriteLine($"Second rational number {numberTwo}");
+            Console.WriteLine($"Third rational number {numberThree}");
+
+            Console.WriteLine($"First and second are equal {numberOne == numberTwo}");
+            Console.WriteLine($"First and third are equal {numberOne == numberThree}");
+            Console.WriteLine($"First and second are NOT equal {numberOne != numberTwo}");
+            Console.WriteLine($"First and third are NOT equal {numberOne != numberThree}");
+
+            Console.WriteLine($"The result of applying Equals to first and second is {numberOne.Equals(numberTwo)}");
+            Console.WriteLine($"The result of applying Equals to first and third is {numberOne.Equals(numberThree)}");
+
+            Console.WriteLine($"Result of {numberOne.ToString()} + {numberTwo.ToString()} is {(numberOne + numberTwo).ToString()}");
+            Console.WriteLine($"Increment of {numberOne.ToString()} is {(++numberOne).ToString()}");
+        }
+
+        static void Main(string[] args)
+        {
+
+            PointOne();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson5/RationalNumber.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Lesson5
+{
+    internal class RationalNumber
+    {
+        private int _numerator;
+        private int _denominator;
+
+        public int Numerator {get {return _numerator;} set { _numerator = value; } }
+        public int Denominator 
+        {
+            get {return _denominator;} 
+            set {
+                if (value == 0)
+                {
+                    throw new ArgumentException("Знаменатель не может быть равен нулю");
+                }
+                else 
+                {
+                    _denominator = value;
+                }
+                } 
+        }
+
+        public RationalNumber(int numerator, int denominator)
+        {
+            _numerator = numerator;
+            if (denominator == 0) 
+            {
+                throw new ArgumentException("Знаменатель не может быть равен нулю");
+            } 
+            else 
+            {
+                _denominator = denominator;
+            }
+            //сгоняем лишнее
+            ShortenTheFraction();
+
+        }
+        /// <summary>
+        /// Вспомогательная функция для сокращения дроби (согнать лишнее). Применять очень аккуратно, только когда дробь полностью сформирована
+        /// </summary>
+        protected void ShortenTheFraction() 
+        {
+            int divisor = (int)BigInteger.GreatestCommonDivisor(_numerator, _denominator);
+            if (Math.Abs(divisor) != 1)
+            {
+                _numerator /= divisor;
+                _denominator /= divisor;
+            }
+        }
+
+        public static bool operator ==(RationalNumber a, RationalNumber b) 
+        { 
+            return (a._denominator*b._numerator == b._denominator*a._numerator);
+        }
+        
+        public static bool operator !=(RationalNumber a, RationalNumber b) 
+        { 
+            return (a._denominator*b._numerator != b._denominator*a._numerator);
+        }
+
+        public override bool Equals(object obj)
+        {
+//            return base.Equals(obj);
+                return Equals(obj as RationalNumber);
+        }
+
+        public bool Equals(RationalNumber someNumber)
+        {
+            return (_denominator * someNumber._numerator == someNumber._denominator * _numerator);
+        }
+
+        public static bool operator >(RationalNumber a, RationalNumber b)
+        {
+            return ((a._numerator * b._denominator - a._denominator*b._numerator)>0);
+        }
+        public static bool operator <(RationalNumber a, RationalNumber b)
+        {
+            return ((a._numerator * b._denominator - a._denominator*b._numerator)<0);
+        }
+        
+        public static bool operator >=(RationalNumber a, RationalNumber b)
+        {
+            return ((a._numerator * b._denominator - a._denominator*b._numerator)>=0);
+        }
+        public static bool operator <=(RationalNumber a, RationalNumber b)
+        {
+            return ((a._numerator * b._denominator - a._denominator*b._numerator)<=0);
+        }
+        
+        public static RationalNumber operator +(RationalNumber a, RationalNumber b)
+        {
+            return new RationalNumber(a._numerator*b._denominator + a._denominator*b._numerator, a._denominator*b._denominator);
+        } 
+        
+        public static RationalNumber operator -(RationalNumber a, RationalNumber b)
+        {
+            return new RationalNumber(a._numerator*b._denominator - a._denominator*b._numerator, a._denominator*b._denominator);
+        }
+
+        public static RationalNumber operator ++(RationalNumber a)
+        {
+            return new RationalNumber(a._numerator + a._denominator,a._denominator);
+        }
+        
+        public static RationalNumber operator --(RationalNumber a)
+        {
+            return new RationalNumber(a._numerator - a._denominator,a._denominator);
+        }
+
+        public override string ToString() 
+        {
+            return _numerator + "/" + _denominator;
+        }
+    }
+}
